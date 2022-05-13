240cf8d5a71b9cecaa166e306b8fd3ec2f2bdca8
Jonathan Steward <jonathan.steward16@gmail.com>
2022-05-12 22:47:25 -0500

    Added credentials to config files

--- src/MFA_proj/GetProperties.java
+++ src/MFA_proj/GetProperties.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package MFA_proj;
+
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  *
- * @author jsteward
+ * @author Jonathan Steward
+ * This class pulls credentials from configuration file to access database or
+ * send a text code to the User for login
  */
 public class GetProperties {
-    
+
+    public String[] getPropValues(int propertyType) {
+        String[] properties;
+                
+        if (propertyType == 0) {
+            properties = new String[3];
+
+            try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+
+                Properties prop = new Properties();
+
+                if (inputStream == null) {
+                    System.out.println("Unable to find config.properties");
+                    return null;
+                }
+
+                // get the property value
+                properties[0] = prop.getProperty("db.url");
+                properties[1] = prop.getProperty("db.username");
+                properties[2] = prop.getProperty("db.password");
+
+            } catch (Exception e) {
+                System.out.println("Exception: " + e);
+            }
+
+            return properties;
+        }
+        else{
+            properties = new String[2];
+            
+            try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+
+                Properties prop = new Properties();
+
+                if (inputStream == null) {
+                    System.out.println("Unable to find config.properties");
+                    return null;
+                }
+
+                // get the property value
+                properties[0] = prop.getProperty("twilio.ACCOUNT_SID");
+                properties[1] = prop.getProperty("twilio.AUTH_TOKEN");
+
+            } catch (Exception e) {
+                System.out.println("Exception: " + e);
+            }
+
+            return properties;
+        }
+    }
 }
